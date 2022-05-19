f75451ac54b88cb335ce7c2631fb5fddd7ae1752
Seongbin Lim <seongbin.lim@polytechnique.edu>
2022-05-19 10:46:36 +0200

    Make utils.py

---
+++ nunet/utils.py
@@ -0,0 +1,96 @@
+import re
+from typing import List, Optional
+from pathlib import Path
+
+import numpy as np
+import torch
+from torchvision import transforms
+
+from .transformer_net import TransformerNet
+from .config import Config, SelfConfig
+
+
+def load_checkpoints(cfg: Config) -> List[TransformerNet]:
+    style_models = []
+    for p in cfg._saved_model_path:
+        model = torch.load(p)
+        state_dict = model['state_dict']
+        for k in list(state_dict.keys()):
+            if re.search(r'in\d+\.running_(mean|var)$', k):
+                del state_dict[k]
+        style_model = TransformerNet()
+        style_model.load_state_dict(state_dict)
+        style_models.append(style_model)
+    return style_models
+
+
+def load_model(
+    cfg: SelfConfig,
+    ind: int = -1
+):
+    print('len(cfg._saved_model_path):', len(cfg._saved_model_path))
+    models = load_checkpoints(cfg)
+
+    common_path = Path(cfg._saved_model_path[0]).parent.stem
+    print('common_path:', {common_path})
+    models_names = [Path(p).stem for p in cfg._saved_model_path]
+
+    for i, name in enumerate(models_names):
+        print(f'{i:3d}, {name}')
+
+    model_name = models_names[ind]
+    print(f'Loading {ind}: {model_name}')
+    nu_net = models[ind]
+    nu_net.cuda()
+    nu_net.eval()
+    return common_path, model_name, nu_net
+
+
+def numpy2torch(
+    array: np.ndarray,
+    device: Optional[str] = None,
+    cuda: Optional[bool] = None,
+) -> torch.Tensor:
+    """Cast an image array to a tensor, ready to be consumed by NU-Net
+
+    Parameters
+    ----------
+    array : numpy.ndarray
+        A numpy array
+    device : Optional
+        If given, cast the tensor to the specified device. Argument to
+        ``torch.Tensor.to()``
+    cuda : Optional
+        Copy the tensor from CPU to GPU. Ignored if `device` is given.
+
+    Returns
+    -------
+    torch.Tensor
+        Data range is assumed to be UINT8 but the actual dtype will be FLOAT32
+        for direct computation.
+
+    """
+    transform = transforms.Compose([
+        transforms.ToTensor(),
+        transforms.Lambda(lambda x: x.mul(255))
+    ])
+    tensor = transform(array)
+    tensor = tensor.unsqueeze(0)
+    if device is not None:
+        tensor = tensor.to(device)
+    elif cuda is not None:
+        tensor = tensor.cuda()
+    return tensor
+
+
+def torch2numpy(
+    tensor: torch.Tensor,
+) -> np.ndarray:
+    """Cast pytorch tensor(s) to numpy array(s)
+
+    """
+    # (1,c,y,x)
+    ch = tensor.size(1)
+    tensor = tensor.detach().cpu().squeeze()
+    array = tensor.permute(1, 2, 0).numpy() if ch == 3 else tensor.numpy()
+    return array
